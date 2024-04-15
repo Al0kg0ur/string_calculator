5387d9f72235cbbc24614f60763aaed01b6cf2e9
Alok Gour <aalok.gour@gmail.com>
2024-04-15 22:50:45 +0530

    Feature# Added a Check for negative numbers.

--- app/models/string_calc.rb
+++ app/models/string_calc.rb
@@ -12,6 +12,12 @@ class StringCalc < ApplicationRecord
     # Convert each part of the array to integer
     integers = numbers_array.map(&:to_i)
 
+    # Check for negative numbers
+    negative_numbers = integers.select { |num| num < 0 }
+    if negative_numbers.any?
+      raise "negative numbers not allowed #{negative_numbers.join(",")}"
+    end
+
     # Calculate the sum of the numbers
     integers.sum
   end
--- test/models/string_calc_test.rb
+++ test/models/string_calc_test.rb
@@ -17,4 +17,12 @@ class StringCalcTest < ActiveSupport::TestCase
     assert_equal 6, @calculator.add("1,5")
   end
 
+  test "negative numbers should throw an exception" do
+    assert_raises(RuntimeError) { @calculator.add("-1,2,-3") }
+  end
+
+  test "exception message should show all negative numbers" do
+    exception = assert_raises(RuntimeError) { @calculator.add("-1,2,-3") }
+    assert_equal "negative numbers not allowed -1,-3", exception.message
+  end
 end
