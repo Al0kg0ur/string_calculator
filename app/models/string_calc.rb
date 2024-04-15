8837cb6d6b3a40905099acacb65fd0424467517d
Alok Gour <aalok.gour@gmail.com>
2024-04-15 22:42:02 +0530

    Feature# Added a test case for empty string, and added a model method to add the string.

--- app/models/string_calc.rb
+++ app/models/string_calc.rb
@@ -1,2 +1,7 @@
 class StringCalc < ApplicationRecord
+  def add(numbers)
+    # Check if the numbers string is empty
+    return 0 if numbers.empty?
+
+  end
 end
--- test/models/string_calc_test.rb
+++ test/models/string_calc_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class StringCalcTest < ActiveSupport::TestCase
+  setup do
+    @calculator = StringCalc.new
+  end
+
+  test "empty string should return 0" do
+    assert_equal 0, @calculator.add("")
+  end
+
 end
